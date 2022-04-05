06aa468f64a54b9c79bddfb621697f18922e7185
gileno <gileno.cr.duarte@gmail.com>
2022-04-05 17:43:11 -0300

    clienado funcao de consumo

--- pokeapi/views.py
+++ pokeapi/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+
+def  index(request):
+    webservice="https://pokeapi.co/api/v2/pokemon"
+    req=requests.get(webservice)
+    try:
+        lista = req.json()
+    except ValueError:
+            print("A resposta não chegou com o formato esperado.")
+
+    dicionario = {}
+    
+    for indice, valor in enumerate(lista):
+        dicionario[indice] = valor
+
+        cnt = {
+            "municipios": dicionario
+        }
+
+    return render(request, "index.html", cnt)
+
+
